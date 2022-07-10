eada30f5239576106c8b8e20fe746e20fc971f04
gbagul <gbagul@avaya.com>
2022-07-10 13:54:31 +0530

    added server file

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,9 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import express, { Application, Request, Response, NextFunction } from 'express'
-import bodyParser from 'body-parser'
-import path from 'path'
-import cors from 'cors'
-require('dotenv').config()
-import { response } from './utils/response'
-import auth from './router/auth'
-import dashboardRouter from './router/dashboard'
-import { logger } from './logger'
+import server from './server'
 
-const app: Application = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
-
-app.use('/', express.static(path.join())) //used in-case of single app, angular is folder which contains UI build
-
-app.use((_: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS')
-  next()
-})
-
-app.use('/auth', auth)
-app.use('/dashboard', dashboardRouter)
-
-app.use('/', (req: Request, res: Response) => {
-  logger.log({
-    level: 'info',
-    message: req.body.message,
-    data: { file: __filename }
-  })
-
-  return response({ res, statusCode: 200, message: 'Hello world!' })
-})
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Log: ~> app is up and running on ~> port-${port} on ENVIRONMENT ~> ${process.env.ENVIRONMENT}`)
 })
 
-module.exports = app
+module.exports = server
---
+++ src/server.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import express, { Application, Request, Response, NextFunction } from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import cors from 'cors'
+require('dotenv').config()
+import { response } from './utils/response'
+import auth from './router/auth'
+import dashboardRouter from './router/dashboard'
+import { logger } from './logger'
+
+const server: Application = express()
+
+server.use(cors())
+server.use(bodyParser.json({ limit: '50mb' }))
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
+
+server.use('/', express.static(path.join()))
+
+server.use((_: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS')
+  next()
+})
+
+server.use('/auth', auth)
+server.use('/dashboard', dashboardRouter)
+
+server.use('/', (req: Request, res: Response) => {
+  logger.log({
+    level: 'info',
+    message: req.body.message,
+    data: { file: __filename }
+  })
+
+  return response({ res, statusCode: 200, message: 'Hello world!' })
+})
+
+export default server
